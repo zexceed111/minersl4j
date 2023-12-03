77455588e27a0da630d4cdc43a00fb9c21d39905
kpmy <petryxa.clever@gmail.com>
2023-12-03 20:36:25 +0300

    no logger

---
+++ src/main/java/ru/practicum/Blockchain.java
@@ -0,0 +1,120 @@
+package ru.practicum;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Optional;
+
+public class Blockchain {
+
+    public static final int HEADER_LENGHT = 6;
+
+    private int nextShift = 0;
+
+    private final int[] nextHeader = new int[HEADER_LENGHT];
+
+    private LinkedList<Block> blocks = new LinkedList<>();
+
+    public Blockchain(){
+        System.out.println("Created a blockchain structure.");
+        Block initialBlock = new Block();
+        initialBlock.header = new int[HEADER_LENGHT];
+        initialBlock.previousHashCode = 0;
+        initialBlock.reward = 0;
+        initialBlock.winner = "practicum";
+        System.out.println("Created initial block "+initialBlock.hashCode());
+        blocks.add(initialBlock);
+        System.out.println("Added initial block");
+        prepareNextHeader();
+    }
+
+    public void prepareNextHeader(){
+        nextShift += (int) Math.round(Math.random() * 10) + 1;
+        System.out.println("Next shift is "+nextShift);
+        for(int n = nextShift; n < nextShift + HEADER_LENGHT; n++){
+            nextHeader[n - nextShift] = calculatePhiNthDigit(n);
+        }
+        System.out.println("next header is " + Arrays.toString(nextHeader));
+    }
+
+    public Optional<Block> checkHeader(String nameCandidate, int[] headerCandidate){
+        System.out.println("Candidate "+ nameCandidate + " wants to check block header " + Arrays.toString(headerCandidate));
+        if(Objects.deepEquals(headerCandidate, nextHeader)) {
+            System.out.println("Candidate " + nameCandidate + " is a winner");
+            Block nextBlock = new Block();
+            nextBlock.reward = 1;
+            nextBlock.winner = nameCandidate;
+            nextBlock.previousHashCode = blocks.getLast().hashCode();
+            nextBlock.header = nextHeader;
+            blocks.add(nextBlock);
+            System.out.println("Created next block "+nextBlock.hashCode());
+            prepareNextHeader();
+            return Optional.of(nextBlock);
+        } else {
+            System.out.println("Candidate " + nameCandidate + " was wrong: "+Arrays.toString(headerCandidate) + " != "+Arrays.toString(nextHeader));
+            return Optional.empty();
+        }
+    }
+
+    public static class Block {
+        private int[] header;
+
+        private int previousHashCode;
+
+        private int reward;
+
+        private String winner;
+
+        public int getReward() {
+            return reward;
+        }
+
+        public String getWinner() {
+            return winner;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Block block = (Block) o;
+            return previousHashCode == block.previousHashCode && reward == block.reward && Arrays.equals(header, block.header) && Objects.equals(winner, block.winner);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = Objects.hash(previousHashCode, reward, winner);
+            result = 31 * result + Arrays.hashCode(header);
+            return result;
+        }
+    }
+
+    private static int calculatePhiNthDigit(int n) {
+        MathContext mc = new MathContext(n + 2); // Extra precision to account for rounding
+        BigDecimal a = BigDecimal.ZERO;
+        BigDecimal b = BigDecimal.ONE;
+        BigDecimal phiApprox;
+
+        // Use Fibonacci sequence to approximate phi
+        for (int i = 0; i < n * n; i++) { // n*n ensures sufficient iterations
+            BigDecimal nextFib = a.add(b);
+            a = b;
+            b = nextFib;
+        }
+        phiApprox = b.divide(a, mc); // Calculate phi approximation
+
+        // Shift decimal point n places
+        BigDecimal tenPowerN = BigDecimal.TEN.pow(n);
+        phiApprox = phiApprox.multiply(tenPowerN);
+
+        // Extract nth digit
+        String phiString = phiApprox.toPlainString();
+        int decimalIndex = phiString.indexOf('.');
+        int nthIndex = decimalIndex - 1; // Account for the index of the decimal point and the integer part "1"
+        char nthDigitChar = phiString.charAt(nthIndex);
+        return Character.getNumericValue(nthDigitChar);
+    }
+
+}
---
+++ src/main/java/ru/practicum/Miner.java
@@ -0,0 +1,74 @@
+package ru.practicum;
+
+import info.schnatterer.mobynamesgenerator.MobyNamesGenerator;
+
+import java.util.*;
+
+public class Miner {
+
+    private static final int CANDIDATES_COUNT = 345;
+
+    private static final int MINING_CYCLES_COUNT = 5432;
+
+    public static void main(String[] args) {
+        System.out.println("Starting a new JRE coin mining session!");
+        Blockchain blockchain = new Blockchain();
+        System.out.println("Created a blockchain");
+        List<Candidate> candidateList = initCandidateList(blockchain, CANDIDATES_COUNT);
+        System.out.println("Created list of candidates");
+
+        System.out.println("Running a mining cycle");
+        Map<String, Integer> rewards = new HashMap<>();
+
+        for (int cycle = 0; cycle < MINING_CYCLES_COUNT; cycle++){
+            System.out.println("Starting mining cycle "+cycle);
+            for (Candidate candidate : candidateList) {
+                System.out.println("Starting candidate try "+candidate.getName());
+                int[] candidateHeader = candidate.getHeader();
+                Optional<Blockchain.Block> maybeBlock = blockchain.checkHeader(candidate.getName(), candidateHeader);
+                if(maybeBlock.isPresent()){
+                    Blockchain.Block block = maybeBlock.get();
+                    rewards.put(candidate.getName(), block.getReward() + rewards.getOrDefault(candidate.getName(), 0));
+                    System.out.println("Candidate "+ candidate.getName()+"win a reward");
+                } else {
+                    System.out.println("Candidate "+ candidate.getName()+" did not win");
+                }
+            }
+        }
+
+
+        rewards.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).forEach(winner -> {
+            System.out.println("Winner is "+winner.getKey()+" with reward "+winner.getValue());
+        });
+    }
+
+    private static List<Candidate> initCandidateList(Blockchain blockchain, int count) {
+        List<Candidate> list = new ArrayList<>();
+        for(int idx = 0; idx < count; idx++){
+            list.add(new Candidate(blockchain));
+        }
+        return list;
+    }
+}
+
+class Candidate {
+    private final Blockchain blockchain;
+    private final String name;
+
+    public Candidate(Blockchain blockchain){
+        this.name = MobyNamesGenerator.getRandomName();
+        this.blockchain = blockchain;
+    }
+
+    public int[] getHeader(){
+        int[] header = new int[Blockchain.HEADER_LENGHT];
+        for (int idx = 0; idx < header.length; idx++){
+            header[idx] = (int) Math.min(Math.max(0, Math.round(Math.random() * 10)), 9);
+        }
+        return header;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
